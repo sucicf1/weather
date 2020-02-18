b322e5a298af0d922d8d8f5c01c2f548ce0fca6e
Ivan Sučić <sucicf1@outlook.com>
2020-02-18 13:58:51 +0100

    All except id

---
+++ weather/weather/Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using static weather.Models.WeatherData;
+
+namespace weather.Controllers
+{
+    public class HomeController : Controller
+    {
+        public WeatherDisplayData GetCityWeatherData(string City)
+        {
+            string appId = "";
+            string url = string.Format("http://api.openweathermap.org/data/2.5/weather?q={0}&units=metric&appid={1}", City, appId);
+
+            using (WebClient client = new WebClient())
+            {
+                string json = client.DownloadString(url);
+                RootObject weatherInfo = (new JavaScriptSerializer()).Deserialize<RootObject>(json);
+                WeatherDisplayData result = new WeatherDisplayData();
+                result.Country = weatherInfo.Sys.Country;
+                result.City = weatherInfo.Name;
+                result.Lat = Convert.ToString(weatherInfo.Coord.Lat);
+                result.Lon = Convert.ToString(weatherInfo.Coord.Lon);
+                result.Pressure = Convert.ToString(weatherInfo.Main.Pressure);
+                result.Humidity = Convert.ToString(weatherInfo.Main.Humidity);
+                result.TempCelsius = Convert.ToString(weatherInfo.Main.Temp);
+                result.TempFahrenheit = Convert.ToString(weatherInfo.Main.Temp * 9 / 5.0 + 32);
+                result.TempFeelsLikeCelsius = Convert.ToString(weatherInfo.Main.Feels_like);
+                result.TempFeelsLikeFahrenheit = Convert.ToString(weatherInfo.Main.Feels_like * 9 / 5.0 + 32);
+                result.TempMaxCelsius = Convert.ToString(weatherInfo.Main.Temp_max);
+                result.TempMaxFahrenheit = Convert.ToString(weatherInfo.Main.Temp_max * 9 / 5.0 + 32);
+                result.TempMinCelsius = Convert.ToString(weatherInfo.Main.Temp_min);
+                result.TempMinFahrenheit = Convert.ToString(weatherInfo.Main.Temp_min * 9 / 5.0 + 32);
+                return result;
+            }
+        }
+        public ActionResult Index(string City = "Sveta Nedelja")
+        {
+            WeatherDisplayData weatherInfo = GetCityWeatherData(City);
+
+            ViewBag.Country = weatherInfo.Country;
+            ViewBag.City = weatherInfo.City;
+            ViewBag.Lat = Convert.ToString(weatherInfo.Lat);
+            ViewBag.Lon = Convert.ToString(weatherInfo.Lon);
+            ViewBag.Pressure = Convert.ToString(weatherInfo.Pressure);
+            ViewBag.Humidity = Convert.ToString(weatherInfo.Humidity);
+            ViewBag.TempCelsius = Convert.ToString(weatherInfo.TempCelsius);
+            ViewBag.TempFahrenheit = Convert.ToString(weatherInfo.TempFahrenheit);
+            ViewBag.TempFeelsLikeCelsius = Convert.ToString(weatherInfo.TempFeelsLikeCelsius);
+            ViewBag.TempFeelsLikeFahrenheit = Convert.ToString(weatherInfo.TempFeelsLikeFahrenheit);
+            ViewBag.TempMaxCelsius = Convert.ToString(weatherInfo.TempMaxCelsius);
+            ViewBag.TempMaxFahrenheit = Convert.ToString(weatherInfo.TempMaxFahrenheit);
+            ViewBag.TempMinCelsius = Convert.ToString(weatherInfo.TempMinCelsius);
+            ViewBag.TempMinFahrenheit = Convert.ToString(weatherInfo.TempMinFahrenheit);
+
+            return View();
+        }
+
+        public ActionResult Download()
+        {
+            WeatherDisplayData svetaNedeljaData = GetCityWeatherData("Sveta Nedelja");
+            WeatherDisplayData splitData = GetCityWeatherData("Split");
+            WeatherDisplayData osijekData = GetCityWeatherData("Osijek");
+            string filePath = Server.MapPath("~/weatherData.txt");
+            FileInfo allDataFile = new FileInfo(filePath);
+            if (allDataFile.Exists) allDataFile.Delete();
+
+            using (StreamWriter w = allDataFile.CreateText())
+            {
+                w.WriteLine(new JavaScriptSerializer().Serialize(svetaNedeljaData));
+                w.WriteLine(new JavaScriptSerializer().Serialize(splitData));
+                w.WriteLine(new JavaScriptSerializer().Serialize(osijekData));
+            }
+            
+            var cd = new System.Net.Mime.ContentDisposition
+            {
+                FileName = filePath,
+                Inline = false,
+            };
+            Response.AppendHeader("Content-Disposition", cd.ToString());
+            return File(allDataFile.OpenRead(), "text/plain");
+        }
+    }
+}
---
+++ weather/weather/Models/WeatherData.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+
+namespace weather.Models
+{
+    public class WeatherData
+    {
+        public class WeatherDisplayData
+        {
+            public string City { get; set; }
+            public string Country { get; set; }
+            public string Lat { get; set; }
+            public string Lon { get; set; }
+            public string Pressure { get; set; }
+            public string Humidity { get; set; }
+            public string TempCelsius { get; set; }
+            public string TempFahrenheit { get; set; }
+            public string TempFeelsLikeCelsius { get; set; }
+            public string TempFeelsLikeFahrenheit { get; set; }
+            public string TempMaxCelsius { get; set; }
+            public string TempMaxFahrenheit { get; set; }
+            public string TempMinCelsius { get; set; }
+            public string TempMinFahrenheit { get; set; }
+        }
+
+        public class Coord
+        {
+            public double Lon { get; set; }
+            public double Lat { get; set; }
+        }
+
+        public class Weather
+        {
+            public int Id { get; set; }
+            public string Main { get; set; }
+            public string Description { get; set; }
+            public string Icon { get; set; }
+        }
+
+        public class Main
+        {
+            public float Temp { get; set; }
+            public float Feels_like { get; set; }
+            public float Temp_min { get; set; }
+            public float Temp_max { get; set; }
+            public float Pressure { get; set; }
+            public float Humidity { get; set; }
+        }
+
+        public class Wind
+        {
+            public double Speed { get; set; }
+            public int Deg { get; set; }
+        }
+
+        public class Clouds
+        {
+            public int All { get; set; }
+        }
+
+        public class Sys
+        {
+            public int Type { get; set; }
+            public int Id { get; set; }
+            public string Country { get; set; }
+            public int Sunrise { get; set; }
+            public int Sunset { get; set; }
+        }
+
+        public class RootObject
+        {
+            public Coord Coord { get; set; }
+            public List<Weather> Weather { get; set; }
+            public string Base { get; set; }
+            public Main Main { get; set; }
+            public int Visibility { get; set; }
+            public Wind Wind { get; set; }
+            public Clouds Clouds { get; set; }
+            public int Dt { get; set; }
+            public Sys Sys { get; set; }
+            public int Timezone { get; set; }
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int Cod { get; set; }
+        }
+    }
+}
